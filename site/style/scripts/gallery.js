6fee32476d5bc8159a63326ed6f11ea0a708c6d1
Pavel_Badzekha <Pavel_Badzekha@EPBYMINW0348>
2021-04-29 22:19:26 +0300

    1234

--- site/style/scripts/gallery.js
+++ site/style/scripts/gallery.js
@@ -1,16 +1,87 @@
 const filterItem = document.querySelector('.items');
 const filterImg = document.querySelectorAll('.image');
+const sizeItem = document.querySelector('.sizes');
+const circleItem = document.querySelector('.circles');
 
+let colorFilter = 'any';
+let sizeFilter = 'any';
+let filterSize = [];
 let filterName = [];
 const images = document.querySelectorAll('.big');
 
 images.forEach((image)=>{
-	console.log('1');
 		image.style.height ='100%';
 		image.style.width ='40%';	
 })
 
 window.onload = () =>{
+	
+	circleItem.onclick = (selectedCircle) =>{
+		if(selectedCircle.target.classList.contains("circle")){
+			if(!selectedCircle.target.classList.contains('cactive')){
+				if(IsColorPicked()){
+					circleItem.querySelector('.cactive').classList.remove('cactive');
+				}
+				selectedCircle.target.classList.add('cactive');			
+				colorFilter = selectedCircle.target.getAttribute('data-name');
+				console.log(colorFilter);
+			}else{
+				selectedCircle.target.classList.remove("cactive");
+				
+				if(circleItem.querySelector('.cactive') == null){
+					colorFilter = 'any';
+					console.log(colorFilter);
+				}
+			}
+		}
+
+		filterImg.forEach((image)=>{
+			FindAllFilters();
+			if((sizeFilter == 'any' || sizeFilter == image.getAttribute('size')) 
+									&& (filterName == "All" || isType(image)) 
+									&& (colorFilter == 'any' || colorFilter == image.getAttribute('color'))){
+				image.classList.remove("hide");
+				image.classList.add("show");
+			}else{
+				image.classList.add("hide");
+				image.classList.remove("show");
+			}
+		});
+	}
+
+	sizeItem.onclick = (selectedSize) =>{
+		if(selectedSize.target.classList.contains("size")){
+			if(!selectedSize.target.classList.contains('sactive')){
+				if(IsSizePicked()){
+					sizeItem.querySelector('.sactive').classList.remove('sactive');
+				}
+				selectedSize.target.classList.add("sactive");
+				sizeFilter = selectedSize.target.getAttribute('data-name');
+				console.log(sizeFilter);
+			}else{
+				selectedSize.target.classList.remove("sactive");
+				if(sizeItem.querySelector('.sactive') == null ){
+					sizeFilter = 'any';
+					console.log(sizeFilter);
+				}
+			}
+		}
+
+		filterImg.forEach((image)=>{
+			FindAllFilters();
+			if((sizeFilter == 'any' || sizeFilter == image.getAttribute('size')) 
+									&& (filterName == "All" || isType(image))
+									&& (colorFilter == 'any' || colorFilter == image.getAttribute('color'))){
+				image.classList.remove("hide");
+				image.classList.add("show");
+			}else{
+				image.classList.add("hide");
+				image.classList.remove("show");
+			}
+		});
+	}
+
+
     filterItem.onclick = (selectedItem)=>{
         if(selectedItem.target.classList.contains("item")){
 			if(filterItem.querySelector('.active').getAttribute('data-name') == 'All'){
@@ -30,7 +101,6 @@ window.onload = () =>{
 				selectedItem.target.classList.remove("active");	
 				FindAllFilters();	
 				if(filterName.length == 0){
-					console.log(filterName.length);
 					filterItem.querySelector("[data-name = 'All']").classList.add('active');
 					FindAllFilters();
 				}
@@ -39,8 +109,9 @@ window.onload = () =>{
 				filterName.push(selectedItem.target.getAttribute("data-name"));
 			}            	
 			filterImg.forEach((image)=>{
-				let filterImages = image.getAttribute("data-name");
-				if(isType(filterImages)|| filterName == "All"){
+				if((sizeFilter == 'any' || sizeFilter == image.getAttribute('size')) 
+									&& (filterName == "All" || isType(image))
+									&& (colorFilter == 'any' || colorFilter == image.getAttribute('color'))){
 					image.classList.remove("hide");
 					image.classList.add("show");
 				}else{
@@ -63,9 +134,21 @@ function FindAllFilters(){
 function isType(image){
 	let isExist = false;
 	filterName.forEach((item)=>{
-		if(image.includes(item)){
-			isExist = true;
+		if(image.getAttribute("data-name").includes(item)){
+			console.log(image.getAttribute("data-name").includes(item));
+			console.log(image.getAttribute('size'));
+			if(sizeFilter == 'any' || image.getAttribute('size') == sizeFilter){
+				isExist = true;
+			}
 		}
 	})
 	return isExist;
+}
+
+function IsSizePicked(){
+	return document.querySelector('.sactive') != null;
+}
+
+function IsColorPicked(){
+	return document.querySelector('.cactive') != null;
 }
